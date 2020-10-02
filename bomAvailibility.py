e44495218733d58058e5f221220d0e2a8b64f543
Douglass Murray <douglass.murray@gmail.com>
2020-10-02 12:52:11 -0400

    Working html searcher with requests and BeautifulSoup modules.

--- bomAvailibility.py
+++ bomAvailibility.py
@@ -5,22 +5,18 @@
 # Run:  python3 bomAvailibility.py /path/to/file/filename.csv
 # Author: Douglass Murray
 
-
-import sys
-import pandas
-from requests_html import HTMLSession
-# import requests
-import json
-import time
 import os
 import ssl
+import sys
+import pandas
+import requests
+from bs4 import BeautifulSoup
 
 if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
         getattr(ssl, '_create_unverified_context', None)):
         ssl._create_default_https_context = ssl._create_unverified_context
 
-octopartAPIKey = '515ffdce74e42367a27b'  # Douglass Murray's account
-testPart = 'SN74S74N'
+testPart = 'ADA4898-1YRDZ' # TODO: get this value from PARTNUM column of csv file
 
 url = 'https://www.digikey.com/products/en?keywords='
 url += testPart
@@ -35,30 +31,11 @@ df.dropna(subset=['PARTNUM'], inplace=True)
 # has attributes: 'Qty', 'Parts', 'MANUF', 'PARTNUM', 'Value'
 print(df['PARTNUM'])
 
-session = HTMLSession()
-html_response = session.get(url)
-print(html_response.html.find('#tr-qtyAvailable ptable-param', first=True))
-container = html_response.html.find("tr-qtyAvailable ptable-param", first=True)
-# list = container.find('td')
-print(container)
-# container = response.html.find('<span>SN74S74N</span>')
-# print(list)
-
-# r = requests.get(url)
-# r.encoding
-# page_source = r.text
+r = requests.get(url)
+r.encoding
+page_source = r.text
 # print(page_source)
-# finder = page_source.find('</html>')  # '<span>SN74S74N</span>'
-# print(finder)
-
-# response = urlopen(url) # stupid python 3
-# response = urllib2.urlopen(url)  # python 2
-# http_content = http.urlopen(url)
-# html_bytes = http_content.read()
-# output = html_bytes.decode('utf-8')
-# print(response)
-# Example from Octopart API documentation
-# print mpn's
-# for result in response['results']:
-    # for item in result['items']:
-        # print(item['mpn'])
+soup = BeautifulSoup(page_source, 'html.parser')
+quantity = soup.find("td", {"class": "tr-qtyAvailable ptable-param"}) # this works in 'Desktop' view
+# mytd = soup.find("td", {"data-atag": "tr-qtyAvailable"}) # this worked once
+print(quantity.get_text())
